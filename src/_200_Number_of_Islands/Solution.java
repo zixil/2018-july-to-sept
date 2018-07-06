fdc2b0f0ff4d9a5a0a23091ef65b12761a5c468a
Liu Zixi <zixil3@uci.edu>
2018-07-06 23:00:30 +0800

    200

--- src/_200_Number_of_Islands/Solution.java
+++ src/_200_Number_of_Islands/Solution.java
@@ -1,24 +1,33 @@
 package _200_Number_of_Islands;
 
 public class Solution {
+    char[][] g;
+    int x, y;
+
     public int numIslands(char[][] grid) {
-        int result = 0;
         if (grid.length < 1) return 0;
-        for (int j = 0; j < grid[0].length; ++j) {
-            if (grid[0][j] == '1' && (j == 0 || grid[0][j - 1] == '0')) {
-                result++;
-            }
-        }
-        for (int i = 1; i < grid.length; ++i) {
-            for (int j = 0; j < grid[i].length; ++j) {
-                if (grid[i][j] == '1' && (j == 0 || grid[i][j - 1] == '0') && (grid[i - 1][j] == '0')) {
+        g = grid;
+        x = grid.length;
+        y = grid[0].length;
+        if (y < 1) return 0;
+        int result = 0;
+        for (int i = 0; i < x; ++i) {
+            for (int j = 0; j < y; ++j) {
+                if (g[i][j] == '1') {
                     result++;
-                }
-                if (grid[i][j] == '1' && (j != 0 && grid[i][j - 1] == '1' && grid[i - 1][j - 1] == '0') && (grid[i - 1][j] == '1')) {
-                    result--;
+                    setNeighbor(i, j);
                 }
             }
         }
         return result;
+
+    }
+
+    public void setNeighbor(int i, int j) {
+        g[i][j] = '0';
+        if (i != 0 && g[i - 1][j] == '1') setNeighbor(i - 1, j);
+        if (j != 0 && g[i][j - 1] == '1') setNeighbor(i, j - 1);
+        if (i != x-1 && g[i + 1][j] == '1') setNeighbor(i + 1, j);
+        if (j != y-1 && g[i][j + 1] == '1') setNeighbor(i, j + 1);
     }
 }
