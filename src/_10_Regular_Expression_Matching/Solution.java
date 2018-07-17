100098f1556af42e0d83235397e3909719b42eb4
Liu Zixi <zixil3@uci.edu>
2018-07-17 18:27:54 +0800

    326

--- src/_10_Regular_Expression_Matching/Solution.java
+++ src/_10_Regular_Expression_Matching/Solution.java
@@ -1,4 +1,7 @@
 package _10_Regular_Expression_Matching;
 
 public class Solution {
+    public boolean isMatch(String s, String p) {
+        return false;
+    }
 }
--- src/_326_Power_of_Three/Solution.java
+++ src/_326_Power_of_Three/Solution.java
@@ -1,4 +1,7 @@
 package _326_Power_of_Three;
 
 public class Solution {
+    public boolean isPowerOfThree(int n) {
+        return (n > 0 && 3486784401L % n == 0);
+    }
 }
