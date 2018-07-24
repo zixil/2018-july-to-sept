278213df82003a482d959cb0c3f3387ef663760e
Liu Zixi <zixil3@uci.edu>
2018-07-24 11:09:05 +0800

    110

---
+++ src/_110_Balanced_Binary_Tree/Solution.java
@@ -0,0 +1,51 @@
+package _110_Balanced_Binary_Tree;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Solution {
+
+    public boolean isBalanced(TreeNode root) {
+        LayerNode newRoot = new LayerNode(root);
+        return isBalanced(newRoot);
+    }
+
+    boolean isBalanced(LayerNode root) {
+        if (root == null) return true;
+        if (root.left == null) {
+            if (root.right == null)
+                return true;
+            else
+                return root.right.getHeight() == 1;
+        } else if (root.right == null)
+            return root.left.getHeight() == 1;
+        int minHeight = root.getHeight()-2;
+        return root.left.getHeight() >= minHeight
+                && root.right.getHeight() >= minHeight
+                && isBalanced(root.left)
+                && isBalanced(root.right);
+    }
+
+
+    class LayerNode {
+        TreeNode node;
+        int height = -1;
+        LayerNode left, right;
+
+        LayerNode(TreeNode node) {
+            this.node = node;
+            left = node.left == null ? null : new LayerNode(node.left);
+            right = node.right == null ? null : new LayerNode(node.right);
+        }
+
+        int getHeight() {
+            if (height != -1) return height;
+            int lH = 0;
+            if (left != null) lH = left.getHeight();
+            int rH = 0;
+            if (right != null) rH = right.getHeight();
+            height = 1 + Math.max(lH, rH);
+            return height;
+        }
+    }
+}
---
+++ src/_110_Balanced_Binary_Tree/TreeNode.java
@@ -0,0 +1,11 @@
+package _110_Balanced_Binary_Tree;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
