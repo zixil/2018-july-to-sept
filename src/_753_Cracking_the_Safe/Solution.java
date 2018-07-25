1fa2f2c901d6d17e7b2268a5d54e271446241581
Liu Zixi <zixil3@uci.edu>
2018-07-25 15:38:18 +0800

    753

--- src/_753_Cracking_the_Safe/Solution.java
+++ src/_753_Cracking_the_Safe/Solution.java
@@ -1,53 +1,49 @@
 package _753_Cracking_the_Safe;
 
-import java.util.Iterator;
-import java.util.LinkedList;
+import java.util.*;
 
 public class Solution {
-    int len;
-    boolean[][] e;
-    JointSet[] v;
+    HashMap<String, Stack<Character>> map=new HashMap<>();
+    int k;
+    char[] A;
+    StringBuilder s;
 
-    public int[] findRedundantConnection(int[][] edges) {
-        len = edges.length;
-        v = new JointSet[len];
-        for (int i = 0; i < len; ++i) {
-            v[i] = new JointSet(i);
+    public String crackSafe(int n, int k) {
+        this.k = k;
+        A = new char[k];
+        for (char i = 0; i < k; ++i) {
+            A[i] = (char) (i + '0');
         }
-        int[] result = null;
-        for (int[] i : edges) {
-            if(v[i[0]-1].sameSet(v[i[1]-1]))
-                result=i;
-            else
-                v[i[0]-1].add(v[i[1]-1]);
+        if(n==1) return String.valueOf(A);
+        getMap("", n - 1);
+        s = new StringBuilder();
+        for (int i = 0; i < n - 1; ++i) {
+            s.append(A[0]);
         }
-        return result;
+        if(k==1) return s.append(A[0]).toString();
+        DFS(s.toString());
+        s.append(s.substring(0,1));
+        return s.toString();
     }
 
-    class JointSet {
-        Integer n;
-        JointSet root;
-
-        JointSet(Integer n) {
-            this.n = n;
-            this.root = null;
-        }
-
-        JointSet getRoot() {
-            if (root == null) return this;
-            while (root.root != null) {
-                root = root.root;
-            }
-            return root;
-        }
-
-        boolean sameSet(JointSet b) {
-            return getRoot() == b.getRoot();
-        }
-
-        void add(JointSet b) {
-            b.getRoot().root = getRoot();
-        }
+    void getMap(String s, int curN) {
+        if (curN == 0) {
+            Stack<Character> stack = new Stack<>();
+            for (int i = '0'; i < k + '0'; ++i) stack.push((char) i);
+            map.put(s, stack);
+        } else for (char i : A)
+            getMap(s + i, curN - 1);
     }
 
+    void DFS(String curS) {
+        char tmp;
+        String tmpS;
+        do {
+            if (map.get(curS).isEmpty()) return;
+            tmp = map.get(curS).pop();
+            tmpS = curS.substring(1) +String.valueOf(tmp);
+        } while (map.get(tmpS).isEmpty());
+        s.append(tmp);
+        DFS(tmpS);
+    }
 }
