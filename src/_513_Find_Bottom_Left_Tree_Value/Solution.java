51d0d5409b447b8aecdeed40442d9c7a9ba41c23
Liu Zixi <zixil3@uci.edu>
2018-08-01 11:26:46 +0800

    513

---
+++ src/_513_Find_Bottom_Left_Tree_Value/Solution.java
@@ -0,0 +1,29 @@
+package _513_Find_Bottom_Left_Tree_Value;
+
+import java.util.LinkedList;
+import java.util.ListIterator;
+import java.util.Queue;
+
+public class Solution {
+    public int findBottomLeftValue(TreeNode root) {
+        LinkedList<TreeNode> curLayer = new LinkedList<>();
+        LinkedList<TreeNode> nextLayer = new LinkedList<>();
+        curLayer.add(root);
+        int layer = 0;
+        ListIterator<TreeNode> iter = curLayer.listIterator();
+        while (true) {
+            TreeNode tmp = iter.next();
+            if (tmp.left != null) nextLayer.add(tmp.left);
+            if (tmp.right != null) nextLayer.add(tmp.right);
+            if (!iter.hasNext()) {
+                if (nextLayer.isEmpty())
+                    return curLayer.getFirst().val;
+                else {
+                    curLayer = nextLayer;
+                    nextLayer = new LinkedList<>();
+                    iter = curLayer.listIterator();
+                }
+            }
+        }
+    }
+}
