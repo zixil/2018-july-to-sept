eafc070041007b2a1df0dbf625b48dce646704ce
Liu Zixi <zixil3@uci.edu>
2018-08-01 17:21:57 +0800

    872

---
+++ src/_872_Leaf_Similar_Trees/Solution.java
@@ -0,0 +1,30 @@
+package _872_Leaf_Similar_Trees;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class Solution {
+    public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+        LinkedList<Integer> l1 = leafSequence(root1), l2 = leafSequence(root2);
+        if (l1.size() != l2.size()) return false;
+        Iterator<Integer> i1 = l1.iterator(), i2 = l2.iterator();
+        while (i1.hasNext()) {
+            if (i1.next() != i2.next()) return false;
+        }
+        return true;
+    }
+
+    LinkedList<Integer> leafSequence(TreeNode root) {
+        LinkedList<Integer> res = new LinkedList<>();
+        if (root.left == null && root.right == null) {
+            res.add(root.val);
+            return res;
+        }
+        if (root.left != null)
+            res.addAll(leafSequence(root.left));
+        if (root.right != null)
+            res.addAll(leafSequence(root.right));
+        return res;
+    }
+
+}
---
+++ src/_872_Leaf_Similar_Trees/TreeNode.java
@@ -0,0 +1,11 @@
+package _872_Leaf_Similar_Trees;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
