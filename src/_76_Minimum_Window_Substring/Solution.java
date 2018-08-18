0e82ff1f79a11651208305436fc305631d0e2348
Liu Zixi <zixil3@uci.edu>
2018-08-18 22:06:11 +0800

    76

--- src/_76_Minimum_Window_Substring/Solution.java
+++ src/_76_Minimum_Window_Substring/Solution.java
@@ -1,6 +1,23 @@
 package _76_Minimum_Window_Substring;
 
 public class Solution {
+    char[] c;
+
     public String minWindow(String s, String t) {
+        int[] map = new int[128];
+        for (char i : t.toCharArray()) map[i]++;
+        c = s.toCharArray();
+        int counter = t.length(), begin = 0, end = 0, d = Integer.MAX_VALUE, head = 0;
+        while (end < c.length) {
+            if (map[c[end++]]-- > 0)
+                counter--;
+            while (counter == 0) {
+                if (end - begin < d)
+                    d = end - (head = begin);
+                if (map[c[begin++]]++ == 0)
+                    counter++;
+            }
+        }
+        return d == Integer.MAX_VALUE ? "" : s.substring(head, head + d);
     }
 }
