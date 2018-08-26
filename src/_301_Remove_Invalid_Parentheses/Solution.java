ee9fd8a12bd6f3c9a2c9822d30fdbb68b3c672bd
Liu Zixi <zixil3@uci.edu>
2018-08-26 19:04:54 +0800

    123

--- src/_301_Remove_Invalid_Parentheses/Solution.java
+++ src/_301_Remove_Invalid_Parentheses/Solution.java
@@ -1,17 +1,11 @@
 package _301_Remove_Invalid_Parentheses;
 
-public class Solution {
-    public List<String> removeInvalidParentheses(String s) {
-        int stack=0;
-        ArrayList<Integer> que=new ArrayList<>();
-        char[] c=s.toCharArray();
-        for(int i=0;i<c.length;++i){
-            if(c[i]=='(') ++stack;
-            if(c[i]==')') {
-                if(stack==0) que.add(i);
-                else --stack;
-            }
+import java.util.ArrayList;
+import java.util.List;
 
-        }
+public class Solution {
+
+    public List<String> removeInvalidParentheses(String s) {
+
     }
 }
